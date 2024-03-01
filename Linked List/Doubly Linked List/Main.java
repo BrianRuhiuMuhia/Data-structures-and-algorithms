595cc81185434bd4d7b798a2b3238d84fb57b3dc
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-03-01 15:56:15 +0300

    delete node

--- Linked List/Doubly Linked List/Main.java
+++ Linked List/Doubly Linked List/Main.java
@@ -5,8 +5,11 @@ list.prepend(50);
 list.prepend(10);
 list.prepend(20);
 list.prepend(30);
+list.append(100);
+list.append(300);
 list.printForward();
-list.printBackward();
+list.deleteNode(10);
+list.printForward();
 }    
 }
 class Node{
@@ -25,6 +28,53 @@ class DoublyLinkedList{
     Node head;
     Node tail;
     int size=0;
+    public Node deleteNode(int data)
+    {
+Node curr=this.head;
+while(curr!=null)
+{
+if(curr.next.data==data){
+break;
+}
+curr=curr.next;
+    }
+    Node deletedNode=curr.next;
+    curr.next=deletedNode.next;
+    deletedNode.next.prev=curr;
+    return deletedNode;
+    }
+    public Node insertANodeAtPos(int data,int pos)
+    {
+        Node node=new Node(data);
+Node curr=this.head;
+int count=0;
+while(count<pos-1 && curr != null)
+{  
+curr=curr.next;
+count++;
+}
+
+node.next=curr.next;
+node.prev=curr;
+curr.next.prev=node;
+curr.next=node;
+        return node;
+    }
+    public void append(int data)
+    {
+        Node node=new Node(data);
+        if(this.head==null)
+{
+this.head=node;
+this.tail=node;
+}
+else{
+    this.tail.next=node;
+    node.prev=this.tail;
+    this.tail=node;
+}
+this.size++;
+    }
    public void prepend(int data)
    {
     Node node=new Node(data);
@@ -42,9 +92,10 @@ this.size++;
    }
    public void printForward(){
     Node curr=this.head;
+    System.out.print("null <-> ");
     while(curr!=null)
     {
-System.out.print(curr + "->");
+System.out.print(curr + " <-> ");
 curr=curr.next;
     }
 System.out.print("null");
@@ -52,9 +103,10 @@ System.out.println("");
    }
    public void printBackward(){
     Node curr=this.tail;
+    System.out.print("null <-> ");
     while(curr!=null)
     {
-System.out.print(curr + "->");
+        System.out.print(curr + " <-> ");
 curr=curr.prev;
     }
 System.out.print("null");
